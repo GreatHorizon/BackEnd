73b2e3ff0925a7b6fe7ef5a7ee27af8999537472
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-03-22 10:46:09 +0300

    Проекты перенесены в одно решение

---
+++ lab2/print_args/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace printArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.Write(args[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace removeDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!" + "\n" + "Usage remove_duplicates.exe < input string >");
+            }
+            else
+            {
+                string resultString;
+                resultString = new string(args[0].Distinct().ToArray());
+                Console.WriteLine(resultString);
+            }
+        }
+    }
+}
